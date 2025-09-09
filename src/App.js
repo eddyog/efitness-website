f0cfa33c14a85d6043fb992037faab513abdd41a
Eddy Gonzalez <oswaldo4495@gmail.com>
2025-09-09 15:04:14 -0600

    Update App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1 className="title">Welcome to Efitness Website</h1>
+        <p className="subtitle">
+          Your journey to fitness starts here 💪
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
